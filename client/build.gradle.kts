d9cd01d48d890ce000e1a3f0697acb3c390f30d7
Robert Eric Reeves <robertericreeves@gmail.com>
2026-03-11 14:52:00 -0500

    Update dependencies to 1.7.1

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -23,7 +23,7 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("com.datadatdat:remote-sdk:1.7.0")
+    implementation("com.datadatdat:remote-sdk:1.7.1")
     implementation("software.amazon.awssdk:auth:2.42.9")
     testImplementation("io.mockk:mockk:1.14.9")
     testImplementation("io.kotlintest:kotlintest-runner-junit5:3.4.2")

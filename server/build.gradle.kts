bf2d640f466919330789501e3f48adeb480a2edb
Robert Eric Reeves <robertericreeves@gmail.com>
2026-04-10 10:28:56 -0500

    Update dependencies to 1.8.3

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -18,7 +18,7 @@ repositories {
 
 dependencies {
 	implementation(kotlin("stdlib"))
-	implementation("com.datadatdat:remote-sdk:1.8.2")
+	implementation("com.datadatdat:remote-sdk:1.8.3")
 	implementation("com.google.code.gson:gson:2.13.2")
 	implementation("software.amazon.awssdk:s3:2.42.31")
 	testImplementation("io.kotlintest:kotlintest-runner-junit5:3.4.2")
